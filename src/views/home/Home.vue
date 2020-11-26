cac5c635f4d6ae40e0ad8004adea993d55036f4c
lvjuntao <ljt@zhanhongit.com>
2020-11-26 17:03:24 +0800

    readme

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -6,14 +6,61 @@
 
     <home-swiper :banners="banners"></home-swiper>
     <recommend-view :recommends="recommends"></recommend-view>
+    <feature-view></feature-view>
+    <tab-control :titles ="['流行','新款','精选']"  class="tab-control"/>
+
+
+    <ul>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li><li>1</li><li>1</li><li>1</li><li>1</li><li>1</li><li>1</li><li>1</li><li>1</li><li>1</li>
+    </ul>
+
   </div>
 </template>
 
 <script>
-import NavBar from "@/components/common/navbar/NavBar";
-import {getHomeMultidata} from "@/network/home";
+import NavBar from "@/components/common/navbar/NavBar.vue";
 import HomeSwiper from "./childComps/HomeSwiper.vue";
 import RecommendView from "./childComps/RecommendView";
+import FeatureView from "./childComps/FeatureView.vue"
+import TabControl from "@/components/content/tabControl/TabControl";
+
+import {getHomeMultidata} from "@/network/home";
+
 export default {
   name: "Home",
   data() {
@@ -25,7 +72,9 @@ export default {
   components: {
     NavBar,
     HomeSwiper,
-    RecommendView
+    RecommendView,
+    FeatureView,
+    TabControl
   },
   created() {
     getHomeMultidata().then(res => {
@@ -39,8 +88,25 @@ export default {
 </script>
 
 <style scoped>
+#home {
+  padding-top: 44px;
+  /*height: 100vh;*/
+  /*position: relative;*/
+}
+
 .home-nav {
   background-color: var(--color-tint);
   color: #f6f6f6;
+
+  position: fixed;
+  left: 0;
+  right: 0;
+  top:0;
+  z-index: 9;
+}
+
+.tab-control {
+  position: sticky;
+  top: 44px;
 }
 </style>
--- src/views/home/childComps/FeatureView.vue
+++ src/views/home/childComps/FeatureView.vue
@@ -1,5 +1,9 @@
 <template>
-
+  <div class="feature">
+    <a href ="https://act.mogujie.com/zzlx67">
+      <img src="@/assets/img/home/recommend_bg.jpg" alt="">
+    </a>
+  </div>
 </template>
 
 <script>
@@ -9,5 +13,9 @@ export default {
 </script>
 
 <style scoped>
+.feature img {
+  width: 100%;
+  /*border-bottom: 8px solid #eee;*/ /*隔断*/
+}
 
 </style>
